b8758c7d4615cbf556cac8e5fb507bdd943a5206
Emmanuel Alawode <emmanuelalawode99@gmail.com>
2018-07-31 17:51:56 -0400

    Updated actions and reducers

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import {connect } from 'react-redux'
 
 class Header extends Component{
+
+  renderContent() {
+    switch (this.props.auth){
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a>Logout</a></li>;
+    }
+
+  }
   render() {
     return (
       <div>
@@ -10,9 +23,7 @@ class Header extends Component{
               Emaily
             </a>
             <ul className='right'>
-              <li>
-                <a> Login With  Google </a>
-              </li>
+              {this.renderContent()}
             </ul>
           </div>
         </nav>
@@ -21,4 +32,9 @@ class Header extends Component{
   }
 }
 
-export default Header;
+function mapStateToProps ({auth}) {
+  return {auth};
+}
+
+
+export default connect(mapStateToProps)(Header);
