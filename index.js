8839699e999c995ce4bf80df4dbb91f7d0788ad6
Emmanuel Alawode <emmanuelalawode99@gmail.com>
2018-08-07 18:53:39 -0400

    prod err 2

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+//putting comments for the changes to the prod 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.rsolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
