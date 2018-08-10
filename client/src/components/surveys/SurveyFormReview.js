f8034715cd96821d2cd9238636d641177fc5ec6d
Emmanuel Alawode <emmanuelalawode99@gmail.com>
2018-08-10 15:05:17 -0400

    posting surveys to the api and redirecting on submit

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,12 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions'
 
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 
   const reviewFields = _.map(formFields, ({name, label}) => {
     return (
@@ -21,10 +24,13 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button
-        className='yellow darken-3 btn-flat'
-        onClick={onCancel}>
+      <button className='yellow white-text darken-3 btn-flat' onClick={onCancel}>
         Back
+      </button>
+
+      <button className='green white-text right btn-flat' onClick={() => submitSurvey(formValues, history)}>
+        Send Survey
+        <i className='material-icons right'>email</i>
       </button>
     </div>
   );
@@ -34,4 +40,4 @@ function mapStateToProps(state) {
   return{ formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
