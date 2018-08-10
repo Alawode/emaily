8e953670e6a2e212be973f0301778e18ad0a4757
Emmanuel Alawode <emmanuelalawode99@gmail.com>
2018-08-09 22:29:52 -0400

    Add a review form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,18 +3,14 @@ import React , {Component} from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import {Link} from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields'
 
-const FIELDS = [
-  { label: 'Survey Title', name: 'title' },
-  { label: 'Subject Line', name: 'subject' },
-  { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' }
-];
 
 class SurveyForm extends Component {
 
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field key={name} component={SurveyField} type='text' label={label} name={name}/>
       );
@@ -24,7 +20,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))} >
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)} >
             {this.renderFields()}
           <Link to='/surveys' className='red btn-flat white-text'>Cancel</Link>
           <button type='submit' className='teal btn-flat right white-text'>
@@ -41,15 +37,20 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  _.each(FIELDS, ({ name }) => {
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(formFields, ({ name }) => {
     if(!values[name]){
       errors[name] = 'You must provide a value';
     }
-  })
+  });
+
+
   return errors;
 }
 
 export default reduxForm({
   validate,
-  form: 'surveyForm'
+  form: 'surveyForm',
+  destroyOnUnmount: false
 })(SurveyForm);
