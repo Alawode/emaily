95c0e8e4b3c2505afc3d46372f701dbc60b102f8
Emmanuel Alawode <emmanuelalawode99@gmail.com>
2018-08-11 18:29:18 -0400

    Executing queries

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,21 +1,52 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
-  })
+  });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    const p = Path.createPath('/api/surveys/:surveyId/:choice');
+
+     _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
     res.send({});
   });
 
-  app.post('/api/surveys', requireLogin, requireCredits,  async (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -27,9 +58,10 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
+    // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
 
-    try{
+    try {
       await mailer.send();
       await survey.save();
       req.user.credits -= 1;
--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,5 +1,5 @@
 function localtunnel {
-  lt -s kjdfjkbaakjdb --port 5000
+  lt -s jsbksbclbckahjlOACX --port 5000
 }
 until localtunnel; do
 echo "localtunnel server crashed"
